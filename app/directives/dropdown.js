64ff5dae774878570aab3c3ae9e6f4b46c0fe783
Jakub Szwacz <jakub@szwacz.com>
2013-10-17 08:25:43 +0200

    dropdown polished

--- app/directives/dropdown.js
+++ app/directives/dropdown.js
@@ -3,10 +3,24 @@
 sputnik.directive('dropdown', function () {
     return {
         restrict: 'C',
-        link: function ($scope, element) {
-            var btn = angular.element(element.children()[0]);
+        link: function ($scope, $element) {
+            
+            var btn = angular.element($element.children()[0]);
+            
             btn.bind('click', function () {
-                element.toggleClass('dropdown--opened');
+                $element.toggleClass('dropdown--opened');
+                process.nextTick(function () {
+                    document.addEventListener('click', hide, false);
+                });
+            });
+            
+            function hide() {
+                $element.toggleClass('dropdown--opened');
+                document.removeEventListener('click', hide, false);
+            }
+            
+            $scope.$on('$destroy', function () {
+                document.removeEventListener('click', hide, false);
             });
         }
     };
